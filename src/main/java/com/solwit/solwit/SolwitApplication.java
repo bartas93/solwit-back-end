16e016550b6687099d7e389f7c274b6e67598fdf
bartas93 <jablonski.bartosz93@gmail.com>
2018-05-08 16:27:00 +0200

    Initial commit with empty spring boot from spring initializer web platform

---
+++ src/main/java/com/solwit/solwit/SolwitApplication.java
@@ -0,0 +1,12 @@
+package com.solwit.solwit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolwitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolwitApplication.class, args);
+	}
+}
